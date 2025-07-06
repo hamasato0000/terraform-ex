eeefa246bc89bd6d94d0df2ce78e9fec0645354a
hamasato0000 <>
2025-07-06 23:50:40 +0900

    update: 変数に型を追加
    TFLintに注意を受けたため。

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,9 +1,11 @@
 variable "is_aws_profile_used" {
   description = "AWSプロファイルを使用するかどうか"
   default     = true
+  type        = bool
 }
 
 variable "aws_profile" {
   description = "使用するAWSプロファイル名"
   default     = "terraform-ex-dev-terraform-user"
+  type        = string
 }
